b43e17e1c829209304cc56690a74736989599d96
Dmytro Ben <dmitriy.ben@gmail.com>
2022-08-07 20:40:41 +0300

    feat(js): added react code

---
+++ download_regions.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import urllib.request
+
+region_ids = {
+  'Вінницька область': 90726,
+  'Волинська область': 71064,
+  'Дніпропетровська область': 101746,
+  'Донецька область': 71973,
+  'Житомирська область': 71245,
+  'Закарпатська область': 72489,
+  'Запорізька область': 71980,
+  'Івано-Франківська область': 72488,
+  'Київська область': 71248,
+  'Кіровоградська область': 101859,
+  'Луганська область': 71971,
+  'Львівська область': 72380,
+  'Миколаївська область': 72635,
+  'Одеська область': 72634,
+  'Полтавська область': 91294,
+  'Рівненська область': 71236,
+  'Сумська область': 71250,
+  'Тернопільська область': 72525,
+  'Харківська область': 71254,
+  'Херсонська область': 71022,
+  'Хмельницька область': 90742,
+  'Черкаська область': 91278,
+  'Чернівецька область': 72526,
+  'Чернігівська область': 71249,
+  'м. Київ': 421866
+}
+
+def download_regions():
+  for region_name,region_id in region_ids.items():
+    print(f"Downloading file for {region_name}...")
+    url = f"https://polygons.openstreetmap.fr/get_geojson.py?id={region_id}&params=0"
+    urllib.request.urlretrieve(url, f"./src/data/regions/{region_name}.json")
+    print(f"Downloading file for {region_name} completed")
+
+def download_alerts():
+    url = f"https://emapa.fra1.cdn.digitaloceanspaces.com/statuses.json"
+    urllib.request.urlretrieve(url, f"./src/data/alerts.js")
+    print(f"Downloading alerts/statuses completed")
+
+if __name__ == '__main__':
+  download_regions()
+  download_alerts()
--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import { MapContainer, TileLayer, GeoJSON, Popup } from 'react-leaflet';
 import './App.css';
+import { alertsStr } from './data/alerts';
+import { useState } from 'react';
+import { geoRegions } from './data/regions';
+
+const copyRight = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const position = [48.7630002, 30.1807396];
 
 function App() {
+  const alertsData = JSON.parse(alertsStr);
+  const [ regions ] = useState(alertsData);
+
+  const getAlertColor = (id) => {
+    return regions.states[id].enabled ? 'orange' : 'green';
+  }
+
+  const getTitle = (regionName) => {
+    const data = regions.states[regionName];
+    const dateStr = data.enabled ? data.enabled_at : data.disabled_at;
+    const prefixStr = data.enabled ? '{regionName} Enabled at ' : 'Disabled at ';
+    const date = new Date(dateStr);
+    const year = date.getFullYear();
+    const day = date.getDate();
+    const month = date.getMonth();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+
+    return `${regionName} ${prefixStr} ${hours}:${minutes} ${day} ${month} ${year}`;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapContainer center={position} zoom={6.25} scrollWheelZoom={false}>
+      <TileLayer
+        attribution={copyRight}
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      { Object.keys(geoRegions).map((regionName) => (
+        <GeoJSON key={regionName} data={geoRegions[regionName]} color={getAlertColor(regionName)} opacity="1">
+          <Popup>
+            {getTitle(regionName)}
+          </Popup>
+        </GeoJSON>
+      )) }
+    </MapContainer>
   );
 }
 
